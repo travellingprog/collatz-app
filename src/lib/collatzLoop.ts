1bd0be97adb9ba3a4487fbd33c10d63ba3e1bab8
Erick Cardenas <erick.mendez@toptal.com>
2023-10-24 15:56:09 -0400

    feat: implement collatz loop creation with mathjs, update UX/UI accordingly

---
+++ src/lib/collatzLoop.ts
@@ -0,0 +1,88 @@
+import { bignumber as big, BigNumber, gcd } from "mathjs";
+
+/**
+ * An assertion for the Typescript type checker to understand that a value should not be null.
+ * Taken from the Typescript docs.
+ */
+function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+  if (val === undefined || val === null) {
+    throw Error(`Expected 'val' to be defined, but received ${val}`);
+  }
+}
+
+/**
+ * The Collatz loop creator, from an algorithm devised by Viktor Zivojinovic.
+ */
+export class Loop {
+  multiplier: BigNumber;
+  denominator: BigNumber;
+  numerator: BigNumber;
+  sequence: BigNumber[];
+
+  constructor(multiplerNum: number, evenSegmentsNum: number[]) {
+    const multiplier = big(multiplerNum);
+    const evenSegments = evenSegmentsNum.map((num) => big(num));
+
+    // if multiplier is even, check that all even segments are the required minimum length
+    if (multiplier.mod(2).equals(0)) {
+      let minimum = 1;
+
+      while (multiplier.mod(big(2).pow(minimum)).equals(0)) {
+        minimum++;
+      }
+
+      if (evenSegments.some((segment) => segment.lessThan(minimum))) {
+        throw Error(
+          `The multiplier value, ${multiplier}, is even` +
+            `and  requires all segments to have at least ${minimum} even numbers`,
+        );
+      }
+    }
+
+    let decreaseCounter = big(0);
+    const totalDecrease = [big(0)];
+    for (const segment of evenSegments) {
+      decreaseCounter = decreaseCounter.add(segment);
+      totalDecrease.push(decreaseCounter);
+    }
+
+    const exponent = evenSegments.length;
+
+    let numerator = big(0);
+    for (let x = 0; x < exponent; x++) {
+      const firstTerm = multiplier.pow(exponent - x - 1);
+      const secondTerm = big(2).pow(totalDecrease[x]);
+      numerator = numerator.add(firstTerm.mul(secondTerm));
+    }
+
+    const lastDecrease = totalDecrease.at(-1);
+    assertIsDefined(lastDecrease);
+    let denominator: BigNumber = big(2)
+      .pow(lastDecrease)
+      .minus(multiplier.pow(exponent));
+
+    const divisor = gcd(numerator, denominator.abs());
+    // ^ abs() use required because of issue https://github.com/josdejong/mathjs/issues/3087
+
+    numerator = numerator.div(divisor);
+    denominator = denominator.div(divisor);
+
+    let loopNumber = numerator;
+    const sequence = [loopNumber];
+
+    for (const segmentNum of evenSegmentsNum) {
+      loopNumber = loopNumber.mul(multiplier).plus(denominator);
+      sequence.push(loopNumber);
+
+      for (let y = 0; y < segmentNum; y++) {
+        loopNumber = loopNumber.div(2).floor();
+        sequence.push(loopNumber);
+      }
+    }
+
+    this.multiplier = multiplier;
+    this.denominator = denominator;
+    this.numerator = numerator;
+    this.sequence = sequence;
+  }
+}
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,16 +1,89 @@
+import { bignumber as big } from "mathjs";
 import Head from "next/head";
 import Link from "next/link";
 import { Inter } from "next/font/google";
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useMemo, useState, ChangeEvent, FormEvent } from "react";
+
+import { Loop } from "@/lib/collatzLoop";
 import styles from "@/styles/Create.module.css";
 
 let idCounter = 1;
 const inter = Inter({ subsets: ["latin"] });
 
+type EvenSegment = {
+  id: number;
+  val: number;
+};
+
+const init = {
+  multiplier: 3,
+  multiplierIsLocked: false,
+  evenSegments: [] as EvenSegment[],
+  loop: null,
+};
+
+/**
+ * The page where the user can create their own loop.
+ */
 export default function Create() {
-  const [multiplier, setMultiplier] = useState(3);
-  const [evenSegments, setEvenSegments] = useState([{ id: idCounter, val: 1 }]);
-  const [showResult, setShowResult] = useState(false);
+  const [multiplier, setMultiplier] = useState(init.multiplier);
+  const [multiplierIsLocked, setMultiplierIsLocked] = useState(
+    init.multiplierIsLocked,
+  );
+  const [evenSegments, setEvenSegments] = useState<EvenSegment[]>(
+    init.evenSegments,
+  );
+  const [loop, setLoop] = useState<Loop | null>(init.loop);
+
+  const multiplerIsEven = useMemo(() => {
+    if (Number.isNaN(multiplier)) {
+      // multiplier is blank
+      return false;
+    }
+
+    return big(multiplier || 0)
+      .mod(2)
+      .equals(0);
+  }, [multiplier]);
+
+  // if the multiplier is even, calculate the required minimum lenght
+  // of every "even numbers" segment in the loop
+  const segmentMin = useMemo(() => {
+    let min = 1;
+    const bigMultiplier = big(multiplier || 0);
+    if (multiplerIsEven && !bigMultiplier.isZero()) {
+      while (bigMultiplier.mod(big(2).pow(min)).equals(0)) {
+        min++;
+      }
+    }
+
+    return min;
+  }, [multiplier, multiplerIsEven]);
+
+  const equation = loop
+    ? `${loop.multiplier}x ${
+        loop.denominator.isNeg() ? "-" : "+"
+      } ${loop.denominator.abs().toFixed(0)}`
+    : "";
+
+  function updateMultiplier() {
+    setMultiplierIsLocked(true);
+    setEvenSegments((segments) => {
+      // add the first segment if required
+      if (segments.length === 0) {
+        return [{ id: idCounter, val: segmentMin }];
+      }
+
+      // otherwise update any existing segment with a value below segmentMin
+      return segments.map(({ id, val }) => {
+        return { id, val: Math.max(val, segmentMin) };
+      });
+    });
+  }
+
+  function onAddSegment() {
+    setEvenSegments((arr) => arr.concat({ id: ++idCounter, val: segmentMin }));
+  }
 
   function onSegmentChange(
     e: ChangeEvent<HTMLInputElement>,
@@ -23,10 +96,6 @@ export default function Create() {
     });
   }
 
-  function onAddSegment() {
-    setEvenSegments((arr) => arr.concat({ id: ++idCounter, val: 1 }));
-  }
-
   function onRemoveSegment(n: number) {
     setEvenSegments((arr) => {
       return [...arr.slice(0, n), ...arr.slice(n + 1)];
@@ -35,11 +104,15 @@ export default function Create() {
 
   function onFinishLoop(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const values = evenSegments.map((segment) => segment.val);
-    let message = `multiplier: ${multiplier}\n`;
-    message += `evenSegments: ${values.join(", ")}`;
-    setShowResult(true);
-    alert(message);
+    const evenSegmentsNum = evenSegments.map((segment) => segment.val);
+    setLoop(new Loop(multiplier, evenSegmentsNum));
+  }
+
+  function onReset() {
+    setMultiplier(init.multiplier);
+    setMultiplierIsLocked(init.multiplierIsLocked);
+    setEvenSegments(init.evenSegments);
+    setLoop(init.loop);
   }
 
   return (
@@ -69,14 +142,50 @@ export default function Create() {
               <label htmlFor="multiplier">Choose your multipler</label>
               <input
                 className={styles.input}
+                disabled={multiplierIsLocked}
                 id="multiplier"
-                min="1"
                 name="multiplier"
                 onChange={(e) => setMultiplier(Number.parseInt(e.target.value))}
                 type="number"
-                value={multiplier}
+                value={Number.isNaN(multiplier) ? "" : multiplier}
               />
+              {multiplierIsLocked ? (
+                <button
+                  className={styles.btn}
+                  onClick={() => setMultiplierIsLocked(false)}
+                  type="button"
+                >
+                  Update
+                </button>
+              ) : (
+                <button
+                  className={styles.btn}
+                  disabled={Number.isNaN(multiplier)}
+                  onClick={updateMultiplier}
+                  type="button"
+                >
+                  Set Multiplier
+                </button>
+              )}
             </div>
+            {!multiplierIsLocked && multiplerIsEven && (
+              <div className={styles.evenWarning}>
+                When the multiplier is even, then each segment in the loop needs
+                a minimum amount of even numbers. <br />
+                For your chosen multiplier, every segment will need to have at
+                least <strong>{segmentMin}</strong> even number
+                {segmentMin > 1 ? "s" : ""}.
+              </div>
+            )}
+            {!multiplierIsLocked &&
+              multiplerIsEven &&
+              evenSegments.some(({ val }) => val < segmentMin) && (
+                <div className={styles.updateWarning}>
+                  Setting this multiplier will automatically increase some of
+                  your segments to a value of {segmentMin}.
+                </div>
+              )}
+
             {evenSegments.map((segment, n) => (
               <div className={styles.formRow} key={segment.id}>
                 <label htmlFor={`even-segment-${segment.id}`}>
@@ -84,15 +193,18 @@ export default function Create() {
                 </label>
                 <input
                   className={styles.input}
+                  disabled={!multiplierIsLocked}
                   id={`even-segment-${segment.id}`}
+                  min={segmentMin}
                   name={`even-segment-${segment.id}`}
                   onChange={(e) => onSegmentChange(e, n, segment.id)}
                   type="number"
-                  value={segment.val}
+                  value={Number.isNaN(segment.val) ? "" : segment.val}
                 />
-                {n > 0 && (
+                {evenSegments.length > 1 && (
                   <button
                     className={styles.btn}
+                    disabled={!multiplierIsLocked}
                     onClick={() => onRemoveSegment(n)}
                     type="button"
                   >
@@ -102,30 +214,65 @@ export default function Create() {
               </div>
             ))}
 
-            <div className={styles.btnRow}>
-              <button
-                className={styles.btn}
-                onClick={onAddSegment}
-                type="button"
-              >
-                Add Segment
-              </button>
-              <button className={styles.btn} type="submit">
-                Finish Loop
-              </button>
-            </div>
+            {evenSegments.length > 0 && (
+              <>
+                <div className={styles.btnRow}>
+                  <button
+                    className={styles.btn}
+                    disabled={!multiplierIsLocked}
+                    onClick={onAddSegment}
+                    type="button"
+                  >
+                    Add Segment
+                  </button>
+                  <button
+                    className={styles.btn}
+                    disabled={
+                      !multiplierIsLocked ||
+                      evenSegments.some(({ val }) => Number.isNaN(val))
+                    }
+                    type="submit"
+                  >
+                    Finish Loop
+                  </button>
+                </div>
+                <div className={styles.resetRow}>
+                  <button
+                    className={styles.btn}
+                    onClick={onReset}
+                    type="button"
+                  >
+                    Reset
+                  </button>
+                </div>
+              </>
+            )}
           </form>
         </section>
 
-        {showResult && (
+        {loop && (
           <section className={styles.section}>
             <div>[visual representation]</div>
-            <div>Your loop is 5X + 971 and looks like this</div>
-            <div>
-              5017
+            <div className={styles.equation}>
+              Your Collatz loop algorithm is:
               <br />
-              26056 <br />
-              ... etc.
+              &bull; if x is odd, calculate <strong>{equation}</strong>
+              <br />
+              &bull; if x is even, calculate <strong>x / 2</strong>
+            </div>
+            <div className={styles.equation}>
+              and starts at the number x=
+              <strong>{loop.numerator.toFixed(0)}</strong>
+            </div>
+            <div className={styles.loop}>
+              {loop.sequence.map((loopElem, idx) => (
+                <div
+                  className={loopElem.mod(2).equals(0) ? styles.even : ""}
+                  key={idx}
+                >
+                  {loopElem.toFixed(0)}
+                </div>
+              ))}
             </div>
           </section>
         )}
